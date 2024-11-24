c8fbd7b41c00d88778e5b6069b2d27971a167571
Lucas Araújo <lucaslopesaraujoufmg@gmail.com>
2024-11-24 15:41:22 -0300

    Correção de erros na função main do Usuário

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from Usuario import Usuario
 
 def main():
 
-    usuario1 = Usuario("Lucas",123)
-    usuario1.login
-    usuario1.modificar_senha
-
+    usuario1 = Usuario("Lucas", "123")
+    usuario1.login()
+    usuario1.modificar_senha()
 
 if __name__ == "__main__":
     main()
--- usuario.py
+++ usuario.py
@@ -14,16 +14,17 @@ class Usuario:
             print("Senha incorreta. Digite uma senha válida!")
             return self.__autenticado
     
-    def modificar_senha(self, nome):
-        senha_atual = input("Digite sua senha atual: ")
+    def modificar_senha(self):
+        senha_atual = input("Para definir uma nova senha, digite sua senha atual: ")
         if senha_atual == self.__senha:
             nova_senha = input("Digite a nova senha: ")
-            confirma_senha = input("Confirme a senha: ")
+            confirma_senha = input("Confirme a nova senha: ")
             if nova_senha != confirma_senha:
                 while(nova_senha != confirma_senha):
                     print("As senhas digitadas não são iguais, tente novamente.")
                     nova_senha = input("Digite a nova senha: ")
-                    confirma_senha = input("Confirme a senha: ")
+                    confirma_senha = input("Confirme a nova senha: ")
+                print("Senha alterada com sucesso!")
             else:
                 self.__senha = nova_senha
                 print("Senha alterada com sucesso!")
