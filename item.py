9a258eb090d5ece985de5b1cd75b421b277737a4
Jonesxtplus <148827346+Jonesxtplus@users.noreply.github.com>
2024-11-27 22:01:05 -0300

    Update item.py

--- item.py
+++ item.py
@@ -1,16 +1,16 @@
 class Item:
-    def __init__(self, nome, un_medida, valor):
+    def __init__(self, nome, estoque, un_medida, preco_un, valor_estoque):
         self.__nome = nome
-        self.__estoque = 0.0
+        self.__estoque = estoque
         self.__un_medida = un_medida
-        self.__valor = valor
-        self.__valor_estoque = self.__valor * self.__estoque
+        self.__preco_un = preco_un
+        self.__valor_estoque = valor_estoque
 
     def mostrar_informacoes(self):
         print(f"Produto: {self.__nome}")
         print(f"Quantidade em estoque: {self.__estoque}")
         print(f"Unidade de medida: {self.__un_medida}")
-        print(f"Valor unitário: {self.__valor} $ por {self.__un_medida}")
+        print(f"Valor unitário: {self.__preco_un} $ por {self.__un_medida}")
         print(f"Valor em estoque: {self.__valor_estoque}")
 
     def entrada_de_estoque(self, quantidade):
@@ -28,29 +28,29 @@ class Item:
 
     @property
     def alterar_valor(self):
-        return self.__valor
+        return self.__preco_un
 
     @alterar_valor.setter
     def alterar_valor(self, valor):
         if valor < 0:
             print("O valor do produto não pode ser negativo")
         else:
-            self.__valor = valor
+            self.__preco_un = valor
             self.atualizar_estoque()
-            return self.__valor
+            return self.__preco_un
 
     def atualizar_estoque(self):
-        self.__valor_estoque = self.__estoque * self.__valor
+        self.__valor_estoque = self.__estoque * self.__preco_un
         return self.__valor_estoque
 
-item1 = Item("Macarrão", "Quilo", 3.0)
-item1.mostrar_informacoes()
-item1.alterar_valor = 5
-item1.entrada_de_estoque(10)
-item1.mostrar_informacoes()
-item1.saida_de_estoque(20)
-item1.mostrar_informacoes()
-item1.saida_de_estoque(5)
-item1.mostrar_informacoes()
-item1.alterar_valor = -10
-item1.mostrar_informacoes()
+# item1 = Item("Macarrão", "Quilo", 3.0)
+# item1.mostrar_informacoes()
+# item1.alterar_valor = 5
+# item1.entrada_de_estoque(10)
+# item1.mostrar_informacoes()
+# item1.saida_de_estoque(20)
+# item1.mostrar_informacoes()
+# item1.saida_de_estoque(5)
+# item1.mostrar_informacoes()
+# item1.alterar_valor = -10
+# item1.mostrar_informacoes()
